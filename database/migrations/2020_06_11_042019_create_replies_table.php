8f25f70879a6974c3d50bf0c85a0d56e4d0afd02
dedisupriadi1612 <dedisupriadi1612@gmail.com>
2020-06-11 22:00:14 +0700

    rename

--- database/migrations/2020_06_11_042019_create_replies_table.php
+++ database/migrations/2020_06_11_042019_create_replies_table.php
@@ -17,9 +17,9 @@ class CreateRepliesTable extends Migration
             $table->id();
             $table->text('body');
 
-            $table->integer('question_id')->unsigned();
+            $table->bigInteger('question_id')->unsigned();
 
-            $table->integer('user_id')->unsigned();
+            $table->bigInteger('user_id');
 
             $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
